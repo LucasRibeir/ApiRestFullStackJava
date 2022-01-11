7dd88d803972850caae9a1cdf01cb7aaa557c247
Lucas Ribeiro <lucas.l.rg@hotmail.com>
2022-01-11 18:06:12 -0300

    Método delete

--- src/main/java/br/com/Lucas/resource/EmployeeController.java
+++ src/main/java/br/com/Lucas/resource/EmployeeController.java
@@ -21,52 +21,50 @@ import br.com.Lucas.service.EmployeeService;
 public class EmployeeController {
 
 	@Autowired
-	
-	private EmployeeService service;
-	
 
-	
-	
+	private EmployeeService service;
+
 	@GetMapping("/employeers")
-	public List<Employee> list(){
+	public List<Employee> list() {
 		return service.listAll();
 	}
-	
-	
+
 	@GetMapping("/employee/{id}")
-	public ResponseEntity<Employee> get(@PathVariable Long id){
+	public ResponseEntity<Employee> get(@PathVariable Long id) {
 		try {
 			Employee employee = service.get(id);
 			return new ResponseEntity<Employee>(employee, HttpStatus.OK);
-			
-		}catch(NoSuchElementException ex) {
-			return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
-			
-		}
-	
-		@PostMapping("/employee/save")
-		public void save(@RequestBody Employee e) {
-			 service.save(e);
-		}
-		
-	
-	
-	}
-	
-	
 
-	@PutMapping("/employee/update/{id}")
-	public ResponseEntity<?> update(@RequestBody Employee e, @PathVariable Long id){
-	
-	try {
-		Employee employeeExist = service.get(id);
-		service.save(e);
-		
-		return new ResponseEntity<>(HttpStatus.OK);
-	}catch (NoSuchElementException ex) {
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		
+		} catch (NoSuchElementException ex) {
+			return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
+
+		}
 	}
+
+	@PostMapping("/employee/save")
+	public void save(@RequestBody Employee e) {
+		service.save(e);
+
+	}
+
+	@DeleteMapping("/employee/delete/{id}")
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+	}
+
+	
+	@PutMapping("/employee/update/{id}")
+	public ResponseEntity<?> update(@RequestBody Employee e, @PathVariable Long id) {
+
+		try {
+			Employee employeeExist = service.get(id);
+			service.save(e);
+
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (NoSuchElementException ex) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		}
 
 	}
 }
